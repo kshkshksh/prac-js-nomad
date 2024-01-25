368472b319172704d703f01ff3861a6221dcea5d
unknown <ksh931101@naver.com>
2024-01-25 10:07:07 +0900

    0125

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginBtnClick() {
+  const username = loginInput.value;
+  console.log(username);
+}
